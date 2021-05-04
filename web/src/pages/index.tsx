feaf18d1594db4e63fea451265fdddcb4255a1b6
Andrew Lyon <anlyon99@gmail.com>
2021-05-03 18:41:09 -0600

    test

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
-import { usePostsQuery } from "../generated/graphql";
-import { Layout } from "../components/Layout";
 import { Link } from "@chakra-ui/react";
+import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
+import { Layout } from "../components/Layout";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
